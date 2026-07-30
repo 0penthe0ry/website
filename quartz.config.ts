de882f441017928e1f208bf1ccc52f4847a72e03
Joanalnu <joanalnu@proton.me>
2026-07-30 09:04:20 +0200

    update baseurl parameter

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "0penthe0ry.github.io",
+    baseUrl: "https://opentheory.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
